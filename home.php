3722e92ebe288d7edfc838a4110778e5363a06d1
NGERESA ENOSH <134959771+NgeresaEnosh@users.noreply.github.com>
2024-05-13 12:03:59 +0300

    home.php

---
+++ home.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> RENTAL HOUSES</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">  
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="main">
+		<div class="logo">
+			<img src="photos/k4.jpeg">
+		</div>
+		
+		 <ul>
+		 <li class="active"><a href="#"><i class="fa fa-home"></i> Home</a></li>
+		
+		 <li><a href="#"></i> HOUSE TYPE <i class="fa fa-caret-down"></i></a>
+		 	<div class="sub-menu">
+		 		<ul>	 
+		 		 <li><a href="#">SINGLE</a></li>
+				 
+		 		 <li><a href="#">2BEDROOM</a></li>
+				  <li><a href="#">3BEDROOM</a></li>
+		 	 </ul>
+		 	</div>
+		 <li><a href="#"></i> TENANTS <i class="fa fa-caret-down"> </i></a>
+		 	<div class="sub-menu">
+		 		<ul>	 
+		 		 <li><a href="#">PROFILE</a></li>
+		 		 <li><a href="#">HISTORY</a></li>
+				  <li><a href="#">REPORT</a></li>
+		 	 </ul>
+		 	</div>
+		
+	
+		 <li><a href="#"></i> REPAIR AND MAINTAINACE <i class="fa fa-caret-down"></i></a>
+		 	<div class="sub-menu">
+		 		<ul>	 
+		 		 <li><a href="#">ELETRICITY.REPAIR</a></li>
+		 		 <li><a href="#">WATER.REPAIR</a></li>
+				  <li><a href="#">GARBAGE</a></li>
+				  <li><a href="#">HOUSERENOVATION</a></li>
+		 	 </ul>
+		 	</div>
+			 	 
+		 		 <li><a href="#">LOGIN</a></li>
+		 		 <li><a href="#">REGISTER</a></li>
+				 
+		 	 </ul>
+		 	</div>
+		 </li>
+		</ul>
+
+		 </div>
+		 <div class="title">
+		 	<h1>RENTAL MANAGEMENT SYSTEM</h1>
+		 </div>
+		 
+		 	</header>
+			
+
+</div>
+</body>
+</html>
